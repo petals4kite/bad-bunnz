e7cbcf00cd6516bb91e88f2f62fc46dc3e65f5a4
Nights <robertsantos.dom@gmail.com>
2025-05-15 00:30:11 +0800

    refactor: reorganize buttons in HeroSection for improved layout

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,5 +1,6 @@
 import { Button, buttonVariants } from "@/components/ui/button"
 import { ChevronRight } from "lucide-react"
+
 import BadbunnzIMG from "@/assets/bad-bunnz.svg"
 
 export default function HeroSection() {
@@ -12,13 +13,25 @@ export default function HeroSection() {
     >
       <div className="px-8 max-w-5xl">
         <h1 className="text-9xl font-bold mb-6 animate-gradient-text">BadBunnz</h1>
-        <Button
-          className={`${buttonVariants({ variant: "outline" })} text-3xl px-8 py-4 transition duration-300 ease-in-out hover:bg-zinc-900 hover:text-white hover:scale-105`}
-        >
-          Mint       
-          <ChevronRight />
-        </Button>
+
+        <div className="flex justify-center">
+          <Button
+            className={`${buttonVariants({ variant: "outline" })} text-3xl px-8 py-4 transition duration-300 ease-in-out hover:bg-zinc-900 hover:text-white hover:scale-105`}
+          >
+            Learn More
+            <ChevronRight />
+          </Button>
+        </div>
       </div>
+      <Button
+        className={`${buttonVariants({ variant: "default" })} absolute left-2.5 bottom-2.5 text-1xl px-4 py-2 transition duration-300 ease-in-out hover:bg-zinc-900 hover:text-white hover:scale-105`}
+      >
+        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">Mint</a>
+        <ChevronRight />
+      </Button>
+
+
     </section>
+
   );
 }
